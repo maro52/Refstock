9e3708f69cee1b2d37a21d80df0e49ebc19a2b1b
michamaro <michamaro52@gmail.com>
2021-01-12 01:22:56 +0900

    [update]fine tuning

--- Refstock/ViewControllers/TopViewController.swift
+++ Refstock/ViewControllers/TopViewController.swift
@@ -19,12 +19,10 @@ class TopViewController: UIViewController, UITableViewDataSource, UITableViewDel
         refTableView.register (UINib(nibName: "RefMemoTableViewCell", bundle: nil),forCellReuseIdentifier:"refMemoTableViewCell")
     }
     
-    // テーブルの行数を指定するメソッド
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
  
-    // ④セルの中身を設定するメソッド（必須）
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "refMemoTableViewCell", for: indexPath) as! RefMemoTableViewCell
         cell.refMemoData(count: indexPath)
